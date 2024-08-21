d91c9a7dbde555417eb9e7f5dabe7587d759def3
NicoIer <NicoIer@163.com>
2024-08-21 20:12:47 +0800

    1

--- DemoClient/NetworkMgr.cs
+++ DemoClient/NetworkMgr.cs
@@ -1,69 +1,69 @@
-using System.Diagnostics;
-using Network;
-using Network.Client;
-
-namespace DemoClient
-{
-    public class NetworkMgr : IClientSystem
-    {
-        public NetworkEntityMgr selfEntityMgr;
-        public Dictionary<int, NetworkEntityMgr> otherEntityMgrs;
-
-        /// <summary>
-        /// 所有的网络实体 自己的和其他人的
-        /// </summary>
-        public Dictionary<uint, NetworkEntity> entities;
-
-        public int connectionId => _client.connectionId;
-        private NetworkClient _client;
-
-        /// <summary>
-        /// 针对不同类型的NetworkComponent的序列化器
-        /// </summary>
-        public NetworkComponentSerializer componentSerializer { get; private set; }
-
-
-        public NetworkMgr()
-        {
-            selfEntityMgr = new NetworkEntityMgr();
-            otherEntityMgrs = new Dictionary<int, NetworkEntityMgr>();
-            entities = new Dictionary<uint, NetworkEntity>();
-            componentSerializer = new NetworkComponentSerializer();
-        }
-
-
-        public void OnInit(NetworkClient t)
-        {
-            _client = t;
-            _client.AddMsgHandler<NetworkEntitySpawn>(OnEntitySpawn);
-            componentSerializer.Register<TransformComponent>();
-        }
-
-        private void OnEntitySpawn(NetworkEntitySpawn spawn)
-        {
-            Debug.Assert(connectionId != 0, "connectionId!=0");
-            Debug.Assert(spawn.id != null, "spawn.id != null");
-            Debug.Assert(spawn.owner != null, "spawn.owner != null");
-            NetworkEntity entity = NetworkEntity.From(spawn.id.Value, spawn.owner.Value, spawn, componentSerializer);
-            entities.Add(entity.id, entity);
-            if (spawn.owner == connectionId)
-            {
-                NetworkLogger.Info($"服务器为自己生成了一个实体:{entity}");
-                selfEntityMgr.Add(entity);
-            }
-            else
-            {
-                if (!otherEntityMgrs.ContainsKey(spawn.owner.Value))
-                {
-                    otherEntityMgrs.Add(spawn.owner.Value, new NetworkEntityMgr());
-                }
-                NetworkLogger.Info($"服务器为其他人[{spawn.owner}]生成了一个实体:{entity}");
-                otherEntityMgrs[spawn.owner.Value].Add(entity);
-            }
-        }
-
-        public void Dispose()
-        {
-        }
-    }
-}
+// using System.Diagnostics;
+// using Network;
+// using Network.Client;
+//
+// namespace DemoClient
+// {
+//     public class NetworkMgr : IClientSystem
+//     {
+//         public NetworkEntityMgr selfEntityMgr;
+//         public Dictionary<int, NetworkEntityMgr> otherEntityMgrs;
+//
+//         /// <summary>
+//         /// 所有的网络实体 自己的和其他人的
+//         /// </summary>
+//         public Dictionary<uint, NetworkEntity> entities;
+//
+//         public int connectionId => _client.connectionId;
+//         private NetworkClient _client;
+//
+//         /// <summary>
+//         /// 针对不同类型的NetworkComponent的序列化器
+//         /// </summary>
+//         public NetworkComponentSerializer componentSerializer { get; private set; }
+//
+//
+//         public NetworkMgr()
+//         {
+//             selfEntityMgr = new NetworkEntityMgr();
+//             otherEntityMgrs = new Dictionary<int, NetworkEntityMgr>();
+//             entities = new Dictionary<uint, NetworkEntity>();
+//             componentSerializer = new NetworkComponentSerializer();
+//         }
+//
+//
+//         public void OnInit(NetworkClient t)
+//         {
+//             _client = t;
+//             _client.AddMsgHandler<NetworkEntitySpawn>(OnEntitySpawn);
+//             componentSerializer.Register<TransformComponent>();
+//         }
+//
+//         private void OnEntitySpawn(NetworkEntitySpawn spawn)
+//         {
+//             Debug.Assert(connectionId != 0, "connectionId!=0");
+//             Debug.Assert(spawn.id != null, "spawn.id != null");
+//             Debug.Assert(spawn.owner != null, "spawn.owner != null");
+//             NetworkEntity entity = NetworkEntity.From(spawn.id.Value, spawn.owner.Value, spawn, componentSerializer);
+//             entities.Add(entity.id, entity);
+//             if (spawn.owner == connectionId)
+//             {
+//                 NetworkLogger.Info($"服务器为自己生成了一个实体:{entity}");
+//                 selfEntityMgr.Add(entity);
+//             }
+//             else
+//             {
+//                 if (!otherEntityMgrs.ContainsKey(spawn.owner.Value))
+//                 {
+//                     otherEntityMgrs.Add(spawn.owner.Value, new NetworkEntityMgr());
+//                 }
+//                 NetworkLogger.Info($"服务器为其他人[{spawn.owner}]生成了一个实体:{entity}");
+//                 otherEntityMgrs[spawn.owner.Value].Add(entity);
+//             }
+//         }
+//
+//         public void Dispose()
+//         {
+//         }
+//     }
+// }
--- DemoClient/Program.cs
+++ DemoClient/Program.cs
@@ -1,43 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// // See https://aka.ms/new-console-template for more information
+//
+// using DemoClient;
+// using Network;
+// using Network.Client;
+// using UnityEngine;
+//
+// TelepathyClientSocket socket = new TelepathyClientSocket();
+// NetworkClient client = new NetworkClient(socket, 60);
+// client.AddSystem<NetworkClientTime>();
+//
+// UriBuilder uriBuilder = new UriBuilder
+// {
+//     Host = "localhost",
+//     Port = 8080,
+// };
+// var task = client.Run(uriBuilder.Uri);
+// client.AddSystem<NetworkMgr>();
+//
+// bool connected = false;
+// socket.OnConnected += () => { connected = true; };
+//
+// await Task.Run(async () =>
+// {
+//     while (!connected)
+//     {
+//         await Task.Delay(100);
+//     }
+// });
+//
+// NetworkBufferPool bufferPool = new NetworkBufferPool();
+// NetworkComponentSerializer componentSerializer = client.GetSystem<NetworkMgr>().componentSerializer;
+// TransformComponent transformComponent = new TransformComponent();
+// transformComponent.pos = new Vector3(0, 0, 0);
+// var buffer = bufferPool.Get();
+// buffer.Reset();
+// NetworkComponentPacket packet = transformComponent.ToDummyPacket(buffer);
+// ArraySegment<NetworkComponentPacket> componentPackets = new ArraySegment<NetworkComponentPacket>(new[] { packet });
+// NetworkEntitySpawn spawn = new NetworkEntitySpawn(null, client.connectionId, componentPackets);
+//
+// client.Send(spawn);
+//
+// await task;
 
-using DemoClient;
-using Network;
-using Network.Client;
-using UnityEngine;
-
-TelepathyClientSocket socket = new TelepathyClientSocket();
-NetworkClient client = new NetworkClient(socket, 60);
-client.AddSystem<NetworkClientTime>();
-
-UriBuilder uriBuilder = new UriBuilder
-{
-    Host = "localhost",
-    Port = 8080,
-};
-var task = client.Run(uriBuilder.Uri);
-client.AddSystem<NetworkMgr>();
-
-bool connected = false;
-socket.OnConnected += () => { connected = true; };
-
-await Task.Run(async () =>
-{
-    while (!connected)
-    {
-        await Task.Delay(100);
-    }
-});
-
-NetworkBufferPool bufferPool = new NetworkBufferPool();
-NetworkComponentSerializer componentSerializer = client.GetSystem<NetworkMgr>().componentSerializer;
-TransformComponent transformComponent = new TransformComponent();
-transformComponent.pos = new Vector3(0, 0, 0);
-var buffer = bufferPool.Get();
-buffer.Reset();
-NetworkComponentPacket packet = transformComponent.ToDummyPacket(buffer);
-ArraySegment<NetworkComponentPacket> componentPackets = new ArraySegment<NetworkComponentPacket>(new[] { packet });
-NetworkEntitySpawn spawn = new NetworkEntitySpawn(null, client.connectionId, componentPackets);
-
-client.Send(spawn);
-
-await task;
+Console.WriteLine("Hello, World!");
